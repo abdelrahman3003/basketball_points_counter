81b2148320124f51a44399886fcfc7ac4099cd95
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2023-10-24 12:19:28 -0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,209 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Points_Counter(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Points_Counter extends StatefulWidget {
+  const Points_Counter({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<Points_Counter> createState() => _Points_CounterState();
+}
+
+class _Points_CounterState extends State<Points_Counter> {
+  int A = 0, B = 0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Colors.yellow,
+        title: const Text(
+          "Points Counter",
+          style: TextStyle(color: Colors.black),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    const Text(
+                      "Team A",
+                      style: TextStyle(color: Colors.black, fontSize: 25),
+                    ),
+                    Text(
+                      "$A",
+                      style:
+                          const TextStyle(color: Colors.black, fontSize: 130),
+                    ),
+                    Column(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              A++;
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 10, left: 10),
+                            alignment: Alignment.center,
+                            width: 100,
+                            height: 30,
+                            color: Colors.yellow,
+                            child: const Text(
+                              "add 1 points",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              A += 2;
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 10, left: 10),
+                            alignment: Alignment.center,
+                            width: 100,
+                            height: 30,
+                            color: Colors.yellow,
+                            child: const Text(
+                              "add 2 points",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              A += 3;
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 10, left: 10),
+                            alignment: Alignment.center,
+                            width: 100,
+                            height: 30,
+                            color: Colors.yellow,
+                            child: const Text(
+                              "add 3 points",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text(
+                      "Team B",
+                      style: TextStyle(color: Colors.black, fontSize: 25),
+                    ),
+                    Text(
+                      "$B",
+                      style:
+                          const TextStyle(color: Colors.black, fontSize: 130),
+                    ),
+                    Column(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              B++;
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 10, left: 10),
+                            alignment: Alignment.center,
+                            width: 100,
+                            height: 30,
+                            color: Colors.yellow,
+                            child: const Text(
+                              "add 1 points",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              B += 2;
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 10, left: 10),
+                            alignment: Alignment.center,
+                            width: 100,
+                            height: 30,
+                            color: Colors.yellow,
+                            child: const Text(
+                              "add 2 points",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              B += 3;
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 10, left: 10),
+                            alignment: Alignment.center,
+                            width: 100,
+                            height: 30,
+                            color: Colors.yellow,
+                            child: const Text(
+                              "add 3 points",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          InkWell(
+            onTap: () {
+              setState(() {
+                A = 0;
+                B = 0;
+              });
+            },
+            child: Container(
+              alignment: Alignment.center,
+              child: Container(
+                alignment: Alignment.center,
+                width: 130,
+                height: 30,
+                color: Colors.yellow,
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
